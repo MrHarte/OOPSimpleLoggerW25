8b6addf8f961cf444c8129a3620f2a8ac8f13809
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-09-30 14:46:05 -0400

    Added timestamp into log message

--- src/SimpleLogger.java
+++ src/SimpleLogger.java
@@ -1,5 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SimpleLogger {
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     public void logThis(String pMessage) {
-        System.out.println(pMessage);
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+        String logEntry = String.format("[%s] %s", timeStamp, pMessage);
+        System.out.println(logEntry);
     }
 }
