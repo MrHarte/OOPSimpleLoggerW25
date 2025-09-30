d40686ff398d18175441b5255c76736c8b18cb0e
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-09-30 15:49:19 -0400

    Fixed merge commit.

--- src/SimpleLogger.java
+++ src/SimpleLogger.java
@@ -6,7 +6,7 @@ public class SimpleLogger {
 
     public void logThis(String pMessage, String pLogLevel) {
         String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_FORMAT));
-        String logEntry = String.format("[%s] [%s] %s !", timeStamp, pLogLevel, pMessage);
+        String logEntry = String.format("[%s] [%s] %s", timeStamp, pLogLevel, pMessage);
         System.out.println(logEntry);
     }
 }
