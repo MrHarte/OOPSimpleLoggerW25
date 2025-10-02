888f86d3a0e4aad88ba0cd505cb89cd3277549ae
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-10-02 14:45:15 -0400

    Finished Singleton

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         SimpleLogger logger = SimpleLogger.getInstance();
-        SimpleLogger logger2 = SimpleLogger.getInstance();
         logger.logThis("Hello", "Info");
     }
 }
